d2afa3ca5bfe22cdac6226fc3d2877526260cda8
Kevin Chen <haokevin.chen@gmail.com>
2019-05-23 12:17:55 +1000

    created model & validations

---
+++ app/models/dose.rb
@@ -0,0 +1,8 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
+  # validates_uniqueness_of :ingredient_id, scope: [:cocktail_id]
+end
