1d9101c46f61ee43ed76a7811636d0efaf076847
Kevin Chen <haokevin.chen@gmail.com>
2019-05-24 16:53:43 +1000

    added image uploader and style

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,10 +1,11 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: %w[show edit update]
+
   def index
     @cocktails = Cocktail.all
   end
 
   def show
-    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
   end
 
@@ -21,9 +22,20 @@ class CocktailsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
   private
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
   def cocktail_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :description, :photo)
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,12 @@
 class Cocktail < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 
   validates :name, presence: true, uniqueness: true
+
+  def sort_name_asc
+    @cocktails = Cocktail.order(:name)
+  end
 end
